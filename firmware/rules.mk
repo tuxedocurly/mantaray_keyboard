62566ac97c73352d5908bde7c7e3cdd66f6aeecc
Tux <3431855+tuxedocurly@users.noreply.github.com>
2025-01-07 17:15:06 -0700

    Create rules.mk

---
+++ firmware/rules.mk
@@ -0,0 +1,3 @@
+OLED_ENABLE = yes  # enables the oled feature
+LTO_ENABLE = yes   # makes the generated hex file smaller at the expense of compile time
+WPM_ENABLE = yes   # enable words per minute counter
