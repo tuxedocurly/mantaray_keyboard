c994e33bf625d4280057808d079e908351bd63ee
Tux <3431855+tuxedocurly@users.noreply.github.com>
2025-01-08 13:32:14 -0700

    Update rules.mk to remove OLED_ENABLE
    Remove OLED_ENABLE from the rules.mk file.
    
    Enabling the OLED feature can be done via keyboard.json and/or the rules.mk file. The multiple declarations are redundant, and to keep the configs clean, the declaration in rules.mk has been removed.

--- firmware/rules.mk
+++ firmware/rules.mk
@@ -1,3 +1,2 @@
-OLED_ENABLE = yes  # enables the oled feature
 LTO_ENABLE = yes   # makes the generated hex file smaller at the expense of compile time
 WPM_ENABLE = yes   # enable words per minute counter
