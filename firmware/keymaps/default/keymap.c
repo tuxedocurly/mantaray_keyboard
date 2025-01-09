956b7685cf0fb88267ad1c2ed01f8568c3286e7b
Tux <3431855+tuxedocurly@users.noreply.github.com>
2025-01-09 10:51:38 -0700

    Update keymap.c CW_TOGG to KC_CAPS
    Change CW_TOGG to KC_CAPS.
    
    Turns out, I don't prefer using caps word over caps lock, so changing this back to something I find to be a more sensible default.

--- firmware/keymaps/default/keymap.c
+++ firmware/keymaps/default/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [0] = LAYOUT(
         KC_ESC,    KC_1,   KC_2,    KC_3,   KC_4,    KC_5,
-        CW_TOGG,   KC_Q,   KC_W,    KC_E,   KC_R,    KC_T,
+        KC_CAPS,   KC_Q,   KC_W,    KC_E,   KC_R,    KC_T,
         SFT_T(KC_BSPC),KC_A,KC_S,   KC_D,   KC_F,    KC_G,
         KC_LGUI,   ALT_T(KC_Z),   KC_X,    KC_C,   KC_V,    KC_B,
         LT(1, KC_ENT),    CTL_T(KC_TAB),
